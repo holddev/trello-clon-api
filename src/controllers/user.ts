3f8ae069d673c7fc7da3f5a168e04ae2b4425554
Usuario6842 <136459003+Usuario6842@users.noreply.github.com>
2025-06-20 17:57:47 -0400

    feat(controller): enhance UserController with error handling and response structure

--- src/controllers/user.ts
+++ src/controllers/user.ts
@@ -1,14 +1,73 @@
+import { User } from "../models/users";
 import { UserRepository } from "../repositories/user";
+import { ControllerResponse } from "../types/types";
 
 export class UserController {
   constructor(private repo: UserRepository) { }
 
-  async findAll() {
-    return await this.repo.findAll();
+  async create(data: User): Promise<ControllerResponse<User>> {
+    try {
+      const result = await this.repo.create(data)
+      return ({
+        ok: true,
+        data: result,
+        status: 201
+      })
+    } catch (error) {
+      console.error("Algo salio mal: ", error)
+      return ({
+        ok: false,
+        data: null,
+        message: "Internal Server Error",
+        status: 500
+      })
+    }
   }
 
-  async findById(id: number) {
-    return await this.repo.findById(id);
+  async findAll(): Promise<ControllerResponse<User[]>> {
+    try {
+      const result = await this.repo.findAll();
+      return ({
+        ok: true,
+        data: result,
+        status: 200
+      })
+    } catch (error) {
+      console.error("Algo salio mal: ", error)
+      return ({
+        ok: false,
+        data: null,
+        message: "Internal Server Error",
+        status: 500
+      })
+    }
+
   }
 
+  async findById(id: string): Promise<ControllerResponse<User>> {
+    try {
+      const result = await this.repo.findById(id);
+      if (!result) {
+        return ({
+          ok: false,
+          data: null,
+          message: "User not found",
+          status: 404
+        })
+      }
+      return ({
+        ok: true,
+        data: result,
+        status: 200
+      })
+
+    } catch (error) {
+      return ({
+        ok: false,
+        data: null,
+        message: "Internal Server Error",
+        status: 500
+      })
+    }
+  }
 }
