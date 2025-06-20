6effc04a9082e9bdfb35d44625abcc1ccbc6a5c6
Usuario6842 <136459003+Usuario6842@users.noreply.github.com>
2025-06-20 17:59:05 -0400

    feat(router): enhance user routes with error handling and response structure

--- src/routers/user.ts
+++ src/routers/user.ts
@@ -1,28 +1,50 @@
 import { Hono } from "hono";
 import { UserRepository } from "../repositories/user";
 import { UserController } from "../controllers/user";
+import { User } from "../models/users";
 
 
 export const userRouter = () => {
 
   const router = new Hono();
 
+  router.post("/", async (c) => {
+    const db = c.get("db");
+    const service = new UserController(new UserRepository(db));
+
+    const data: User = await c.req.json()
+    const result = await service.create(data)
+
+    if (!result.ok) {
+      return c.json({ error: result.message }, result.status);
+    }
+
+    return c.json(result.data, result.status);
+  })
+
   router.get("/", async (c) => {
     const db = c.get("db");
     const service = new UserController(new UserRepository(db));
 
-    const users = await service.findAll();
-    return c.json(users);
+    const result = await service.findAll();
+    if (!result.ok) {
+      return c.json({ error: result.message }, result.status);
+    }
+    return c.json(result.data, result.status);
   });
 
   router.get("/:id", async (c) => {
     const db = c.get("db");
     const service = new UserController(new UserRepository(db));
 
-    const id = parseInt(c.req.param("id"));
-    const user = await service.findById(id);
-    if (!user) return c.json({ error: "User not found" }, 404);
-    return c.json(user);
+    const id = c.req.param("id");
+
+    const result = await service.findById(id);
+    if (!result.ok) {
+      return c.json({ error: result.message }, result.status);
+    }
+
+    return c.json(result.data, result.status);
   });
 
   return router;
