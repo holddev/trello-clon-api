09cf9405e82a59b7763499e56c757f0e293d9e82
Usuario6842 <136459003+Usuario6842@users.noreply.github.com>
2025-06-23 21:51:48 -0400

    feat(controller): implement BoardController for managing board operations

---
+++ src/controllers/boards.ts
@@ -0,0 +1,65 @@
+import { Board, newBoard } from "../models/boards";
+import { BoardRepository } from "../repositories/boards";
+import { ControllerResponse } from "../types/types";
+
+export class BoardController {
+  constructor(private boardRepository: BoardRepository) { }
+
+  async create(data: newBoard): Promise<ControllerResponse<Board>> {
+    try {
+      const result = await this.boardRepository.create(data);
+      return { ok: true, data: result, status: 201 };
+    } catch (error) {
+      return { ok: false, data: null, message: "Internal Server Error", status: 500 };
+    }
+  }
+
+  async findAll(userId: string): Promise<ControllerResponse<(Board)[]>> {
+    try {
+      const result = await this.boardRepository.findAll(userId);
+      if (!result || result.length === 0) {
+        return { ok: false, data: null, message: "No boards found", status: 404 };
+      }
+      return { ok: true, data: result, status: 200 };
+    } catch (error) {
+      return { ok: false, data: null, message: "Internal Server Error", status: 500 };
+    }
+  }
+
+  async updateField(userId: string, id: number, data: Partial<Board>): Promise<ControllerResponse<Board>> {
+    try {
+      const result = await this.boardRepository.updateField(userId, id, data);
+      if (!result) {
+        return { ok: false, data: null, message: "Board not found", status: 404 };
+      }
+      return { ok: true, data: result, status: 200 };
+    } catch (error) {
+      return { ok: false, data: null, message: "Internal Server Error", status: 500 };
+    }
+  }
+
+  async findByIdWithDetails(userId: string, id: number): Promise<ControllerResponse<Board | null>> {
+    try {
+      const result = await this.boardRepository.findByIdWithColumnsAndTasks(userId, id);
+      if (!result) {
+        return { ok: false, data: null, message: "Board not found", status: 404 };
+      }
+      return { ok: true, data: result, status: 200 };
+    } catch (error) {
+      return { ok: false, data: null, message: "Internal Server Error", status: 500 };
+    }
+  }
+
+  async delete(userId: string, id: number): Promise<ControllerResponse<Board>> {
+    try {
+      const result = await this.boardRepository.delete(userId, id);
+      if (!result) {
+        return { ok: false, data: null, message: "Board not found", status: 404 };
+      }
+      return { ok: true, data: result, message: "Board deleted successfully", status: 200 };
+    } catch (error) {
+      return { ok: false, data: null, message: "Internal Server Error", status: 500 };
+    }
+  }
+
+}
