6184066101cbf76c0bf541cc1f13effd08d54a90
Usuario6842 <136459003+Usuario6842@users.noreply.github.com>
2025-06-25 23:51:07 -0400

    feat(types): define ControllerResponse interface and Status type

--- src/types/types.ts
+++ src/types/types.ts
@@ -2,3 +2,13 @@ export type Env = {
   DB_URL: string
   DB_TOKEN: string
 }
+
+type Status = 200 | 201 | 400 | 404 | 500;
+
+export interface ControllerResponse<T> {
+  ok: boolean;
+  data: T | null;
+  message?: string;
+  status: Status;
+}
+
