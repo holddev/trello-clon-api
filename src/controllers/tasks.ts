3fde2c85961c29cc2adba15850c99ce7a7a5c010
Usuario6842 <136459003+Usuario6842@users.noreply.github.com>
2025-06-30 16:27:28 -0400

    feat(controller): add TaskController with CRUD operations for tasks

---
+++ src/controllers/tasks.ts
@@ -0,0 +1,38 @@
+import { newTask, Task } from "../models/tasks";
+import { TaskRepository } from "../repositories/tasks";
+import { ControllerResponse } from "../types/types";
+
+export class TaskController {
+  constructor(private taskRepository: TaskRepository) { }
+
+  async create(userId: string, data: newTask): Promise<ControllerResponse<Task>> {
+    try {
+      const result = await this.taskRepository.create(userId, data);
+      return { ok: true, data: result, status: 201 };
+    } catch (error) {
+      console.error("Error creating task:", error);
+      return { ok: false, data: null, message: "Internal Server Error", status: 500 };
+    }
+  }
+
+  async update(userId: string, taskId: number, data: Partial<Task>): Promise<ControllerResponse<Task>> {
+    try {
+      const result = await this.taskRepository.update(userId, taskId, data);
+      return { ok: true, data: result, status: 200 };
+    } catch (error) {
+      console.error("Error updating task:", error);
+      return { ok: false, data: null, message: "Internal Server Error", status: 500 };
+    }
+  }
+
+  async delete(userId: string, taskId: number): Promise<ControllerResponse<null>> {
+    try {
+      await this.taskRepository.delete(userId, taskId);
+      return { ok: true, data: null, message: "Task deleted successfully", status: 200 };
+    } catch (error) {
+      console.error("Error deleting task:", error);
+      return { ok: false, data: null, message: "Internal Server Error", status: 500 };
+    }
+  }
+
+}
