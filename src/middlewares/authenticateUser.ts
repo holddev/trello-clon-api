7cfe5660331ca1ad7a97616af77b97ee61c9c9a4
Usuario6842 <136459003+Usuario6842@users.noreply.github.com>
2025-07-02 15:43:55 -0400

    feat(auth): implement user authentication middleware with error handling

---
+++ src/middlewares/authenticateUser.ts
@@ -0,0 +1,27 @@
+import { Context, Next } from "hono";
+import { UnauthorizedError } from "../errors/error";
+import { UserRepository } from "../repositories/user";
+import { handleControllerError } from "../utils/utils";
+
+export const authenticateUser = async (c: Context, next: Next) => {
+  const userId = c.req.query('userId') as string;
+  const db = c.get('db');
+  const repo = new UserRepository(db);
+
+  if (!userId) {
+    return c.json({ error: 'User ID is required' }, 400);
+  }
+
+  try {
+    const result = await repo.findById(userId);
+
+    if (!result) {
+      throw new UnauthorizedError('Unauthorized: user does not exist.');
+    }
+
+    await next();
+  } catch (error) {
+    const { message, status } = handleControllerError(error);
+    return c.json({ error: message }, status);
+  }
+};
