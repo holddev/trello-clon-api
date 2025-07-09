fe5d345ffd9f50bbc138916c6893255760c9801b
Usuario6842 <136459003+Usuario6842@users.noreply.github.com>
2025-07-09 18:08:14 -0400

    feat(auth): refactor user authentication to use token verification and create user if not found

--- src/middlewares/authenticateUser.ts
+++ src/middlewares/authenticateUser.ts
@@ -2,23 +2,57 @@ import { Context, Next } from "hono";
 import { UnauthorizedError } from "../errors/error";
 import { UserRepository } from "../repositories/user";
 import { handleControllerError } from "../utils/utils";
+import { createClerkClient, verifyToken } from "@clerk/backend";
+import { Env } from "../types/types";
+import { env } from "hono/adapter";
+
 
 export const authenticateUser = async (c: Context, next: Next) => {
-  const userId = c.req.query('userId') as string;
-  const db = c.get('db');
-  const repo = new UserRepository(db);
+  const config = env<Env>(c, 'workerd')
 
-  if (!userId) {
-    return c.json({ error: 'User ID is required' }, 400);
-  }
+  const authHeader = c.req.header('Authorization');
 
   try {
-    const result = await repo.findById(userId);
+    const db = c.get("db")
 
-    if (!result) {
+    if (!authHeader || !authHeader.startsWith('Bearer ')) {
+      throw new UnauthorizedError('Se requiere un token de autenticación.');
+    }
+
+    const token = authHeader.split(' ')[1];
+
+    if (!token) {
+      throw new UnauthorizedError('Token de autenticación no proporcionado.');
+    }
+
+
+    const client = await verifyToken(token, {
+      secretKey: config.CLERK_SECRET_KEY,
+    });
+
+    if (!client || !client.sub) {
+      throw new UnauthorizedError('Token inválido o usuario no encontrado.');
+    }
+    const repo = new UserRepository(db);
+    let user = await repo.findById(client.sub as string);
+
+    if (!user) {
+      // 🔽 Intentar crear el usuario directamente
+      const clerk = createClerkClient({ secretKey: config.CLERK_SECRET_KEY });
+      const clerkUser = await clerk.users.getUser(client.sub);
+
+      user = await repo.create({
+        id: clerkUser.id,
+        name: `${clerkUser.firstName ?? ''} ${clerkUser.lastName ?? ''}`.trim(),
+        email: clerkUser.emailAddresses[0]?.emailAddress ?? '',
+      });
+    }
+
+    if (!user) {
       throw new UnauthorizedError('Unauthorized: user does not exist.');
     }
-    c.set('userId', result.id);
+
+    c.set('userId', user.id);
     await next();
   } catch (error) {
     const { message, status } = handleControllerError(error);
